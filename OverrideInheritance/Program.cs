0a96bf9663951841aa3db77e59fded96c68bac74
SUBHAM TRIPATHI <subhamtripathi2002gniot@gmail.com>
2025-05-26 05:26:10 +0530

    Commit changes

--- OverrideInheritance/Program.cs
+++ OverrideInheritance/Program.cs
@@ -1,12 +1,38 @@
 ﻿namespace OverrideInheritance
 {
 
+    class Books
+    {
+        public virtual void Subject()
+        {
+            Console.WriteLine("College Books");
+        }
+    }
 
+    class Maths : Books
+    {
+        public override void Subject()
+        {
+            Console.WriteLine("Maths question solved");
+        }
+    }
+
+    class Physics : Books
+    {
+        public override void Subject()
+        {
+            Console.WriteLine("Science problem solved");
+        }
+    }
      class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Books book1 = new Maths();
+            Books book2 = new Physics();
+
+            book1.Subject();
+            book2.Subject();
         }
     }
 }
